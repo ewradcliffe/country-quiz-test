87476d7fa9d911245fe58596e737c808395999b5
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-27 10:03:43 +0000

    Add while loop to generate buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,28 @@
 let countries = ['austria', 'belgium', 'bulgaria', 'cyprus', 'denmark', 'france', 'germany', 'greece', 'greenland', 'hungary'];
 
 
-
-
 /*Function to diplay question*/
 function displayQuestion (event) {
     /*Randomly selects a country (random selector taken from https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/)*/
     let  currentCountry = countries[Math.floor(Math.random()*countries.length)];
     /*Displays the Map Silhouette*/
     let showMap = document.getElementById('quiz-area');
-    showMap.innerHTML = `<p>It worked!</p><img src = "assets/images/${currentCountry}.png" alt = "Silhouette of country.">`;
+    showMap.innerHTML = `<img src = "assets/images/${currentCountry}.png" alt = "Silhouette of country.">`;
+
+    /*To generate the buttons*/ 
+    let countryButtons = [];
+    /*adds the current Country to the button array*/
+    countryButtons.push(currentCountry);
+    console.log(countryButtons);
+    /*Generate three random countries, checks to see if it is the array, and if not adds it to the array*/
+    while (countryButtons.length <= 3) {
+        newCountry = countries[Math.floor(Math.random()*countries.length)];
+        countryButtons.push(newCountry);
+        
+    }
+    console.log(countryButtons);
+    /*Adds the selected country to the array at a random location.*/ 
+
 }
 
 
