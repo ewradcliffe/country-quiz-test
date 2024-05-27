7b98bcd35ffcf2be829a3e0a97dc0ce64af98a73
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-27 08:45:08 +0000

    Add basic JS.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/*List of countries part of the quiz*/
+let countries = [austria, belgium, bulgaria, cyprus, denmark, france, germany, greece, greenland, hungary];
+
+/*Function to diplay question*/
+
+
+/*Event Listener which triggers display question*/
+let askMeAQuestion = document.getElementById('show-me-a-country!');
+askMeAQuestion.addEventListener('click', askQuestion);
