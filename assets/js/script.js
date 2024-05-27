156ce507e771c72606fdfb915d1dca7f23ea91e5
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-27 09:11:39 +0000

    Add code to randomly select a country.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@
 let countries = ['austria', 'belgium', 'bulgaria', 'cyprus', 'denmark', 'france', 'germany', 'greece', 'greenland', 'hungary'];
 
 
+
+
 /*Function to diplay question*/
 function displayQuestion (event) {
+    /*Randomly selects a country (random selector taken from https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/)*/
+    let  currentCountry = countries[Math.floor(Math.random()*countries.length)];
+    /*Displays the Map Silhouette*/
     let showMap = document.getElementById('quiz-area');
-    showMap.innerHTML = `<p>It worked!</p><img src = "assets/images/${countries[0]}.png">`;
+    showMap.innerHTML = `<p>It worked!</p><img src = "assets/images/${currentCountry}.png" alt = "Silhouette of country.">`;
 }
 
 
