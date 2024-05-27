db7dbfe636a43c297b86c09e2c542a0d677c4d88
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-27 09:03:03 +0000

    Add function to display country.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 /*List of countries part of the quiz*/
-let countries = [austria, belgium, bulgaria, cyprus, denmark, france, germany, greece, greenland, hungary];
+let countries = ['austria', 'belgium', 'bulgaria', 'cyprus', 'denmark', 'france', 'germany', 'greece', 'greenland', 'hungary'];
+
 
 /*Function to diplay question*/
+function displayQuestion (event) {
+    let showMap = document.getElementById('quiz-area');
+    showMap.innerHTML = `<p>It worked!</p><img src = "assets/images/${countries[0]}.png">`;
+}
 
 
 /*Event Listener which triggers display question*/
 let askMeAQuestion = document.getElementById('show-me-a-country!');
-askMeAQuestion.addEventListener('click', askQuestion);
+askMeAQuestion.addEventListener('click', displayQuestion);
